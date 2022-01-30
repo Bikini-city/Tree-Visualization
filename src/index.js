6c5861577caa6293869d23ee103d3dfb122d6630
parkeunsik <qkrdmstlr3@naver.com>
2022-01-30 11:29:46 -0500

    [feat] GlobalStyle 구현
    - 전역적으로 스타일 구현 및 적용

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import GlobalStyle from './style/GlobalStyle';
 import App from './App';
 
 ReactDom.render(
   <BrowserRouter>
+    <GlobalStyle />
     <App />
   </BrowserRouter>,
   document.getElementById('app'),
---
+++ src/style/GlobalStyle.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Global, css } from '@emotion/react';
+
+function GlobalStyle() {
+  return (
+    <Global
+      styles={css`
+        * {
+          box-sizing: border-box;
+          font-family: 'Comfortaa', cursive;
+        }
+        html,
+        body,
+        #root {
+          margin: 0;
+          height: 100%;
+          font-size: 62.5%;
+        }
+        a {
+          text-decoration: none;
+          color: inherit;
+        }
+        button {
+          outline: none;
+          cursor: pointer;
+        }
+        li {
+          list-style: none;
+        }
+      `}
+    />
+  );
+}
+
+export default GlobalStyle;
