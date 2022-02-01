36d0f7a3fe4bdadf810233d533e2eeb55a209572
parkeunsik <qkrdmstlr3@naver.com>
2022-02-01 10:42:28 -0500

    [feat] Marker 생성 / 제거 구현
    - 지도에 마커 생성 / 제거 코드 구현

---
+++ src/components/UI/Map/Marker.js
@@ -0,0 +1,11 @@
+import mapboxgl from 'mapbox-gl';
+
+export const addMarkersToMap = ({ markers, map }) => {
+  markers.forEach((marker) => {
+    new mapboxgl.Marker().setLngLat([marker.lng, marker.lat]).addTo(map);
+  });
+};
+
+export const removeMarkersToMap = ({ markers }) => {
+  markers.forEach((marker) => marker.remove());
+};
