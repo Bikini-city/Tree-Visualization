32eee4bd1979433335712235a2ad4e40bc8aaff4
parkeunsik <qkrdmstlr3@naver.com>
2022-02-02 11:17:40 -0500

    [fix] 더미마커로 마커기능 테스트
    - addMarkersToMap에서 map.getMap()으로 지도 가져옴

--- src/components/UI/Map/Marker.js
+++ src/components/UI/Map/Marker.js
@@ -2,7 +2,7 @@ import mapboxgl from 'mapbox-gl';
 
 export const addMarkersToMap = ({ markers, map }) => {
   markers.forEach((marker) => {
-    new mapboxgl.Marker().setLngLat([marker.lng, marker.lat]).addTo(map);
+    new mapboxgl.Marker().setLngLat([marker.lng, marker.lat]).addTo(map.getMap());
   });
 };
 
--- src/pages/Main/index.jsx
+++ src/pages/Main/index.jsx
@@ -1,10 +1,21 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Map from '../../components/UI/Map';
+import { addMarkersToMap } from '../../components/UI/Map/Marker';
+
 import Icon from '../../components/UI/Icon';
 import * as Style from './styled';
 
+// FIXME: remove
+const DUMMY_MARKERS = [{ lng: -86.89871737888747, lat: 40.41866254968954 }];
+
 function Main() {
   const [map, setMap] = useState();
+
+  useEffect(() => {
+    if (map) {
+      addMarkersToMap({ map, markers: DUMMY_MARKERS });
+    }
+  }, [map]);
 
   return (
     <Style.Container>
