c3a7eaa74405cfebc1803dc7dac9bd9a2e0f2a04
parkeunsik <qkrdmstlr3@naver.com>
2022-02-02 22:10:23 -0500

    [test] Icon UI에 대한 테스트코드 구현
    - 아이콘 UI테스트위한 스토리북파일 구현

---
+++ src/components/UI/Icon/Icon.stories.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import Icon from '.';
+import { IconList, IconItem } from './styled';
+
+export default {
+  title: 'UI/Icon',
+  component: Icon,
+};
+
+export const icon = () => (
+  <IconList>
+    <IconItem>
+      <Icon icon="home" />
+    </IconItem>
+    <IconItem>
+      <Icon icon="plus" />
+    </IconItem>
+    <IconItem>
+      <Icon icon="purdue" />
+    </IconItem>
+    <IconItem>
+      <Icon icon="statistics" />
+    </IconItem>
+  </IconList>
+);
--- src/components/UI/Icon/styled.js
+++ src/components/UI/Icon/styled.js
@@ -3,3 +3,16 @@ import styled from '@emotion/styled';
 export const Container = styled.div`
   width: ${(props) => props.width};
 `;
+
+/**
+ * storybook
+ */
+export const IconList = styled.ul`
+  display: flex;
+  padding: 0;
+`;
+
+export const IconItem = styled.li`
+  width: 5rem;
+  margin: 1rem;
+`;
