53bff51bafed6906c31b61d1e6f0c8b674ced83f
parkeunsik <qkrdmstlr3@naver.com>
2022-02-10 21:00:32 -0500

    [feat] connect api and main page
    - connect getDatasAPI and main page
      - When main page render, get dataset

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import axios from 'axios';
 import GlobalStyle from './style/GlobalStyle';
 import App from './App';
+
+axios.defaults.baseURL = `${process.env.SERVER_URL}`;
 
 ReactDom.render(
   <BrowserRouter>
