5c3377fdbf2279459d834422762f823d2b6291b8
parkeunsik <qkrdmstlr3@naver.com>
2022-02-17 15:20:32 -0500

    [refactor] integrate parsing util functions
    - integrate two functions in utils/data.js
    - remove unnecessary dummy data

--- src/utils/data.js
+++ src/utils/data.js
@@ -6,17 +6,18 @@ const getDateGap = (toDate, fromDate) => {
   return dateGap;
 };
 
-export const makeDataToChartFormat = (dataset, fromDate, toDate, gap) => {
+export const parseData = (dataset, fromDate, toDate, gap) => {
   const dateGap = getDateGap(toDate, fromDate);
   if (dateGap < gap) return [];
 
-  const result = [];
+  const chartFormatData = [];
+  const graphFormatData = [];
   const treeInfos = []; // 각 날짜에 해당하는 나무들 저장
   const innerCount = Math.ceil(dateGap / gap);
 
   new Array(innerCount).fill(0).forEach((_, index) => {
     treeInfos.push({ broken: 0, down: 0 });
-    result.push({
+    chartFormatData.push({
       name: `${index}`,
       percentage: 100 / innerCount,
       color: Color.gray,
@@ -26,6 +27,11 @@ export const makeDataToChartFormat = (dataset, fromDate, toDate, gap) => {
         { name: 'down', percentage: 0, color: Color.black, textColor: Color.gold },
       ],
     });
+    graphFormatData.push({
+      name: `${index}`,
+      broken: 0,
+      down: 0,
+    });
   });
 
   dataset.forEach((data) => {
@@ -34,39 +40,16 @@ export const makeDataToChartFormat = (dataset, fromDate, toDate, gap) => {
     const index = Math.floor(gapOfDataAndFromDate / gap);
     treeInfos[index].broken += data.broken;
     treeInfos[index].down += data.down;
+    graphFormatData[index].broken += data.broken;
+    graphFormatData[index].down += data.down;
   });
 
   treeInfos.forEach((info, index) => {
     if (info.broken === 0 && info.down === 0) return;
 
-    result[index].data[0].percentage = Math.floor((info.broken / (info.broken + info.down)) * 100); // broken tree
-    result[index].data[1].percentage = Math.floor((info.down / (info.broken + info.down)) * 100); // down tree
+    chartFormatData[index].data[0].percentage = Math.floor((info.broken / (info.broken + info.down)) * 100); // broken tree
+    chartFormatData[index].data[1].percentage = Math.floor((info.down / (info.broken + info.down)) * 100); // down tree
   });
 
-  return result;
-};
-
-export const makeDataToGraphFormat = (dataset, fromDate, toDate, gap) => {
-  const dateGap = getDateGap(toDate, fromDate);
-  if (dateGap < gap) return [];
-
-  const result = [];
-  const innerCount = Math.ceil(dateGap / gap);
-
-  new Array(innerCount).fill(0).forEach((_, index) => {
-    result.push({
-      name: `${index}`,
-      broken: 0,
-      down: 0,
-    });
-  });
-
-  dataset.forEach((data) => {
-    const gapOfDataAndFromDate = getDateGap(data.date, fromDate);
-    const index = Math.floor(gapOfDataAndFromDate / gap);
-    result[index].broken += data.broken;
-    result[index].down += data.down;
-  });
-
-  return result;
+  return { chartFormatData, graphFormatData };
 };
