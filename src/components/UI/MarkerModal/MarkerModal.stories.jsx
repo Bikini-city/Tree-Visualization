4cf11543d9550fe5308633cc387f70502e855148
parkeunsik <qkrdmstlr3@naver.com>
2022-02-19 23:21:19 -0500

    [test] make mockup function for dummy daya
    - For test, change mockup variable to function

--- src/components/UI/MarkerModal/MarkerModal.stories.jsx
+++ src/components/UI/MarkerModal/MarkerModal.stories.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { action } from '@storybook/addon-actions';
-import { dataMockup } from '../../../__mocks__/data';
+import { makeDataMockup } from '../../../__mocks__/data';
 import MarkerModal from '.';
 
 export default {
@@ -10,4 +10,4 @@ export default {
 
 const closeModalAction = action('click marker');
 
-export const markerModal = () => <MarkerModal data={dataMockup} closeModal={closeModalAction} />;
+export const markerModal = () => <MarkerModal data={makeDataMockup()} closeModal={closeModalAction} />;
