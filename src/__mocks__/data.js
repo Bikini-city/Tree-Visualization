3a91b8ce68e808f63bab000fdf0f4fab2bf427f8
parkeunsik <qkrdmstlr3@naver.com>
2022-02-19 19:30:10 -0500

    [test] refactor MarkerModal Component storybook
    - change props name
    - add mockup data file

---
+++ src/__mocks__/data.js
@@ -0,0 +1,9 @@
+export const dataMockup = {
+  id: 13,
+  lat: '40.4240988275343500',
+  lng: '-86.9177794290636400',
+  src: '/media/%EC%84%9C%EB%AA%85_gtv1QSR.jpg',
+  date: '2022-02-09',
+  broken: 4,
+  down: 3,
+};
--- src/components/UI/MarkerModal/MarkerModal.stories.jsx
+++ src/components/UI/MarkerModal/MarkerModal.stories.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { action } from '@storybook/addon-actions';
+import { dataMockup } from '../../../__mocks__/data';
 import MarkerModal from '.';
 
 export default {
@@ -7,7 +8,6 @@ export default {
   component: MarkerModal,
 };
 
-const DUMMY_IMAGE = 'https://avatars.githubusercontent.com/u/26402298?v=4';
 const closeModalAction = action('click marker');
 
-export const markerModal = () => <MarkerModal imgSrc={DUMMY_IMAGE} closeModal={closeModalAction} />;
+export const markerModal = () => <MarkerModal data={dataMockup} closeModal={closeModalAction} />;
