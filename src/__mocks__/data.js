275118873d04229b80c5f7d52075082be73e8180
parkeunsik <qkrdmstlr3@naver.com>
2022-02-19 23:21:19 -0500

    [test] make mockup function for dummy daya
    - For test, change mockup variable to function

--- src/__mocks__/data.js
+++ src/__mocks__/data.js
@@ -1,9 +1,17 @@
-export const dataMockup = {
-  id: 13,
-  lat: '40.4240988275343500',
-  lng: '-86.9177794290636400',
-  src: '/media/%EC%84%9C%EB%AA%85_gtv1QSR.jpg',
-  date: '2022-02-09',
-  broken: 4,
-  down: 3,
-};
+export const makeDataMockup = ({
+  id = Math.floor(Math.random() * 100),
+  lat = 40.42409882753435,
+  lng = -86.91777942906364,
+  src = '/media/%EC%84%9C%EB%AA%85_gtv1QSR.jpg',
+  date = '2022-02-09',
+  broken = 4,
+  down = 3,
+} = {}) => ({
+  id,
+  lat,
+  lng,
+  src,
+  date,
+  broken,
+  down,
+});
